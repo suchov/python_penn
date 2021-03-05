f63c350131a373fbbb2ab9951828b19c573aa1e3
Artem Sychov <artem.suchov@gmail.com>
2021-03-05 18:59:46 +0200

    add even number count

--- data_structures.py
+++ data_structures.py
@@ -1,5 +1,13 @@
-numbers = [1, 3, 5, 4, 6, 6, 13, 15]
+numbers = [1, 3, 5, 4, 6, 6, 13, 15, 10, 8]
 
+even_numbers = []
+
+even_count = 0
 # just loop through numbers
 for number in numbers:
-    print(number)
+    if (number % 2 == 0):
+        even_numbers.append(number)
+        even_count += 1
+print(even_numbers)
+print(f"There are {even_count} even numbers in the list.")
+print(f"There are {len(even_numbers)} even numbers in the list.")
