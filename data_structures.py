f584bcf4901d84591e7a8c95a838e6d91df03db0
Artem Sychov <artem.suchov@gmail.com>
2021-03-14 14:35:14 +0200

    Iteration for and for + range

--- data_structures.py
+++ data_structures.py
@@ -11,3 +11,32 @@ for number in numbers:
 print(even_numbers)
 print(f"There are {even_count} even numbers in the list.")
 print(f"There are {len(even_numbers)} even numbers in the list.")
+
+# find the minimum value in the list
+min_list_numbers = [5, 3, 8, -1, -2.2, 0, -4]
+
+# define min number variable
+min_number = min_list_numbers[0]
+
+# iterate over the list and find the minimum value
+for number in min_list_numbers:
+    if number < min_number:
+        min_number = number
+
+print(min_number, "is the min number")
+
+#Prompt and print - prompt user for the first name
+# using the for loop:
+# - print each letter of the name(on the same line)
+# - count each letter in the name
+# Print the count of letters in the name
+
+name = input("Give me your name! ")
+letter_count = 0
+
+for n in name:
+    letter_count += 1
+    print(n, end=" ")
+
+print(" ")
+print(letter_count, "- the count of letters in the name", name)
---
+++ for_loops_using_range.py
@@ -0,0 +1,29 @@
+# for loops using range
+#Format: range(start, up_to, step)
+# - start and step are both optional
+# - up_to means "up to but not including" the value
+# - can only use integers! (no floats)
+
+# Iterate over the sequence of 10 numbers, 0 - 9
+for x in range(10):
+    print(x)
+print("")
+
+# Iterate over sequence of 10 numbers, 9 - 0
+for x in range(0, 10):
+    print(x)
+print("")
+
+# Iterate over sequence of 6 numbers 1 - 6
+for x in range(1, 7):
+    print(x)
+print("")
+
+# Iterate over sequence of 5 numbers, 0 - 28, skipping every 6 numbers
+for x in range(0, 30, 7):
+    print(x)
+print("")
+
+# Iterate over sequence of 6 numbers, counting backwards from 5 - 0
+
+# Find the numbers between 1 and 120 that are odd
